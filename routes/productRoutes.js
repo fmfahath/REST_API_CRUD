4a50d746e68fa679d672c923dcf3947bcaaa937c
fmfahath <fmfahath.dev@gmail.com>
2025-06-21 15:17:57 +0530

    Fix: updated request methods (PUT, DELETE) in productRoutes.js and corrected typos

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,7 +6,7 @@ const productRouter = express.Router();
 productRouter.post('/register', registerProduct)
 productRouter.get('/', getAllProducts);
 productRouter.get('/:id', getProductById);
-productRouter.post('/update-product/:id', updateProduct);
-productRouter.post('/delete-product/:id', deleteProduct);
+productRouter.put('/update-product/:id', updateProduct);
+productRouter.delete('/delete-product/:id', deleteProduct);
 
 export default productRouter;
